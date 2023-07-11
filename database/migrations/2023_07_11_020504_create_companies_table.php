322560bb2bfbf40bb3ff12a815a35d41d810be49
deudesdelroy <deudes15@hotmail.com>
2023-07-10 21:24:59 -0500

    style: type hinting for company migration

--- database/migrations/2023_07_11_020504_create_companies_table.php
+++ database/migrations/2023_07_11_020504_create_companies_table.php
@@ -6,12 +6,8 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
+    
+    public function up(): void
     {
         Schema::create('companies', function (Blueprint $table) {
             $table->id();
@@ -19,12 +15,7 @@ return new class extends Migration
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
+    public function down(): void
     {
         Schema::dropIfExists('companies');
     }
